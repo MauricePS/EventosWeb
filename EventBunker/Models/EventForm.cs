c5a45d88119dde4feeda14594b4c3f7ea789156b
MauricePS <mportilhosoares@yahoo.com>
2021-09-15 13:57:37 -0300

    Bootstrap theme

---
+++ EventBunker/Models/EventForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EventBunker.Models
+{
+    public class EventForm
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
